c9d2e9c98b04b30d0ec63a9fac710ca6fa389a6d
J. Andrés <jn.ndrs96@gmail.com>
2019-05-25 19:00:06 +0200

    [UPDATE] - Datepicker

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_components/routes/routes.dart';
 import 'package:flutter_components/src/alert_page.dart';
 // import 'package:flutter_components/src/home_temp.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,9 +15,16 @@ class MyApp extends StatelessWidget {
       routes: getAppRoutes(),
       onGenerateRoute: (RouteSettings settings) {
         return MaterialPageRoute(
-          builder: (BuildContext context) => AlertPage()
-        );
+            builder: (BuildContext context) => AlertPage());
       },
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English
+        const Locale('es', 'ES'), // Spanish
+      ],
     );
   }
 }
