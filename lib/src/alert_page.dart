9f3092b59ca51bfa90a9affb025f1a2b87d02b15
J. Andrés <jn.ndrs96@gmail.com>
2019-05-25 15:47:32 +0200

    [UPDATE] - AvatarPage icons and images

--- lib/src/alert_page.dart
+++ lib/src/alert_page.dart
@@ -7,6 +7,17 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Show alert'),
+          onPressed: () {
+            _showAlert(context);
+          },
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
@@ -14,5 +25,32 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+            title: Text('Title'),
+            content: Column(mainAxisSize: MainAxisSize.min, children: [
+              Text('Content'),
+              FlutterLogo(
+                size: 100.0,
+              )
+            ]),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cancel'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              FlatButton(
+                child: Text('Accept'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          );
+        });
   }
 }
--- lib/src/avatar_page.dart
+++ lib/src/avatar_page.dart
@@ -6,12 +6,41 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(0),
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              backgroundColor: Colors.lightGreen,
+              child: Icon(
+                Icons.ac_unit,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('JA'),
+              backgroundColor: Colors.purple,
+            ),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
           Navigator.pop(context);
         },
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              'https://s24597.pcdn.co/wp-content/uploads/2017/11/Tube-sock-snowman-craft-project-13.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          height: 400,
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
