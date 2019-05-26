4a2d9797192a06d2134b1d6c281d41c6e467a57b
J. Andrés <jn.ndrs96@gmail.com>
2019-05-26 13:34:54 +0200

    [ADD] - Slider

---
+++ lib/src/slider_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  SliderPage({Key key}) : super(key: key);
+
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double _sliderValue = 125.0;
+  double _boxOpacity = 125.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sliders Page'),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _createSlider(),
+            Divider(),
+            _createSquare(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _createSlider() {
+    return Slider(
+      activeColor: Colors.purple,
+      label: 'Opacity of the box',
+      value: _sliderValue,
+      min: 0.0,
+      max: 255.0,
+      onChanged: (value) {
+        setState(() {
+          _sliderValue = value;
+          _boxOpacity = value;
+          print(value);
+        });
+      },
+    );
+  }
+
+  Widget _createSquare() {
+    return Container(
+        margin: EdgeInsets.only(top: 50.0),
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20.0),
+            color: Color.fromRGBO(125, 245, 55, _boxOpacity / 255.0)),
+    );
+  }
+}
